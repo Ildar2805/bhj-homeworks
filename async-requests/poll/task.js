7a84529b17e775869596c7e56829945720773fff
Ildar <ildar2805@gmail.com>
2023-01-12 08:18:23 +0300

    Async-requests v2

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -6,48 +6,52 @@ xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
 xhr.send();
 
 xhr.onload = function() {
-    const parseData = JSON.parse(xhr.response);
-    title.innerHTML = parseData.data.title;
+    if (xhr.status != 200) {
+        alert(`Ошибка ${xhr.status}: ${xhr.statusText}`);
+    } else {
+        const parseData = JSON.parse(xhr.response);
+        title.innerHTML = parseData.data.title;
 
-    for (let i=0; i < parseData.data.answers.length; i++) {
-        let button = document.createElement('button');
-        button.innerHTML = parseData.data.answers[i];
-        button.className = 'poll__answer';
-        answers.appendChild(button);
+        for (let i=0; i < parseData.data.answers.length; i++) {
+            let button = document.createElement('button');
+            button.innerHTML = parseData.data.answers[i];
+            button.className = 'poll__answer';
+            answers.appendChild(button);
+        }
+
+        const buttons = Array.from(document.querySelectorAll('.poll__answer'));
+        
+        buttons.forEach(b => {
+            b.addEventListener('click', (e) => {
+                alert('Спасибо, ваш голос засчитан!');
+                
+                let xhrResult = new XMLHttpRequest();
+                xhrResult.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
+                xhrResult.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                const index = parseData.data.answers.indexOf(e.target.textContent);
+                const body = 'vote=' + parseData.id + '&answer=' + index;
+                xhrResult.send(body);
+
+                xhrResult.onload = function() {
+                    answers.remove()
+
+                    const result = JSON.parse(xhrResult.response);
+                    console.log(result);
+
+                    let total = [];
+                    for (let i=0; i < result.stat.length; i++) {
+                        total.push(result.stat[i].votes);
+                    }
+                    const sum = total.reduce((partialSum, a) => partialSum + a, 0);
+
+                    for (let i=0; i < result.stat.length; i++) {
+                        let div = document.createElement('div');
+                        div.innerHTML = result.stat[i].answer + ': ' + (result.stat[i].votes/sum * 100).toFixed(2) + '%';
+                        div.className = 'poll__answer';
+                        title.appendChild(div);
+                    } 
+                };
+            });
+        });
     }
-
-    const buttons = Array.from(document.querySelectorAll('.poll__answer'));
-    
-    buttons.forEach(b => {
-        b.addEventListener('click', (e) => {
-            alert('Спасибо, ваш голос засчитан!');
-            
-            let xhrResult = new XMLHttpRequest();
-            xhrResult.open('POST', 'https://students.netoservices.ru/nestjs-backend/poll');
-            xhrResult.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            const index = parseData.data.answers.indexOf(e.target.textContent);
-            const body = 'vote=' + parseData.id + '&answer=' + index;
-            xhrResult.send(body);
-
-            xhrResult.onload = function() {
-                answers.remove()
-
-                const result = JSON.parse(xhrResult.response);
-                console.log(result)
-
-                let total = []
-                for (let i=0; i < result.stat.length; i++) {
-                    total.push(result.stat[i].votes);
-                }
-                const sum = total.reduce((partialSum, a) => partialSum + a, 0);
-
-                for (let i=0; i < result.stat.length; i++) {
-                    let div = document.createElement('div');
-                    div.innerHTML = result.stat[i].answer + ': ' + (result.stat[i].votes/sum * 100).toFixed(2) + '%';
-                    div.className = 'poll__answer';
-                    title.appendChild(div);
-                } 
-            }
-        })
-    })
 }
--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -4,20 +4,20 @@ const form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     const xhr = new XMLHttpRequest();
-    xhr.addEventListener('readystatechange', () => {
-        if (xhr.readyState === xhr.OPENED) {
-            progress.value = 0.25;
-        } else if (xhr.readyState === xhr.HEADERS_RECEIVED) {
-            progress.value = 0.5;
-        } else if (xhr.readyState === xhr.LOADING) {
-            progress.value = 0.75;
-        } else if (xhr.readyState === xhr.DONE) {
-            progress.value = 1;
+
+    xhr.upload.onprogress = function(event) {
+        progress.value = (event.loaded / event.total).toFixed(1);
+    }
+    xhr.onloadend = function() {
+        if (xhr.status == 201) {
+          alert("Данные успешно отправлены.");
+        } else {
+          alert("Ошибка " + this.status);
         }
-    })
+    };
     xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
     const formData = new FormData(form);
     xhr.send(formData);
-})
+});
 
 
